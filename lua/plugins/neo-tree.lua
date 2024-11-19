697aab1485949db3bbb9ac36d73bbe0089e0482c
Afrando Sharein Ramadhan <afrandormd@gmail.com>
2024-11-19 15:51:06 +0700

    setting neo-tree agar menampilkan semua file tersembunyi

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,19 +1,37 @@
 return {
-  -- neo-tree (your existing configuration)
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    config = function()
-      require("neo-tree").setup({
-        window = {
-          position = "right",
-          width = 30,
+  "nvim-neo-tree/neo-tree.nvim",
+  config = function()
+    require("neo-tree").setup({
+      window = {
+        position = "right",
+        width = 30,
+      },
+      -- Pastikan filter diatur untuk menampilkan file tersembunyi
+      filters = {
+        dotfiles = false, -- Menampilkan file yang dimulai dengan titik
+        custom = {}, -- Menampilkan semua folder
+      },
+      -- Menonaktifkan pengaturan lain yang mungkin menyebabkan file tersembunyi
+      filesystem = {
+        filtered_items = {
+          visible = true, -- Menampilkan semua file dan folder
         },
-      })
-    end,
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    },
+      },
+      render = {
+        highlight_opened_files = "name",
+        icons = {
+          show = {
+            file = true,
+            folder = true,
+            folder_arrow = true,
+          },
+        },
+      },
+    })
+  end,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
   },
 }
